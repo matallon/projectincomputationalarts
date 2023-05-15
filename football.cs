27e2c0aa918086059fd9a14f48ac22be1b623839
matallon <83500084+matallon@users.noreply.github.com>
2023-05-15 13:46:10 +0100

    Update football.cs

--- football.cs
+++ football.cs
@@ -2,6 +2,8 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+//adds more movement to objects in the scene if the player collides with them. Most notibly is on a football object. 
+
 public class football : MonoBehaviour
 {
     public float speed = 10f;
