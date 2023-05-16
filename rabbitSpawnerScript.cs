e1389cb4f54bf87383652c963ea20d3bf34dd9fd
matallon <83500084+matallon@users.noreply.github.com>
2023-05-16 11:08:26 +0100

    Update rabbitSpawnerScript.cs

--- rabbitSpawnerScript.cs
+++ rabbitSpawnerScript.cs
@@ -2,9 +2,10 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+//instantiates 10 rabbits onto the navmesh, rabbits are assigned in the inspector
+
 public class rabbitSpawnerScript : MonoBehaviour
 {
-
     public GameObject rabbit; 
 
     // Start is called before the first frame update
