be8073dda7cd045d7d86449f8beeb087a8ec6d3c
matallon <83500084+matallon@users.noreply.github.com>
2023-05-16 11:14:54 +0100

    Update terrainDetailDistance.cs

--- terrainDetailDistance.cs
+++ terrainDetailDistance.cs
@@ -2,12 +2,14 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+//means the terrain details, such as grass, crops and flowers can be seen from a greater distance
+//than the unity UI typically allows. 
+
 public class terrainDetailDistance : MonoBehaviour
 {
     public Terrain terrain1; 
     public Terrain terrain2;
 
- 
     // Update is called once per frame
     void Update()
     {
